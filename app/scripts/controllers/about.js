26e7258004235d945c40d4287254361ab92b3fa5
franciolucio <franciolucio@gmail.com>
2016-09-22 17:15:26 -0300

    crean proyecto frontend

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name holaMundoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the holaMundoApp
+ */
+angular.module('holaMundoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
